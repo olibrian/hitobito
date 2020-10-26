db5bd10fdd54c0ff05ff506edebb74cf446c9432
Nils Rauch <rauch@puzzle.ch>
2020-10-26 09:22:26 +0100

    Expand specs and doublette checker

--- app/domain/people/doublette_checker.rb
+++ app/domain/people/doublette_checker.rb
@@ -2,6 +2,27 @@
 
 class People::DoubletteChecker
   def check
+    Person.find_each do |person|
+      doublette = person_doublette_finder.find({ first_name: person.first_name,
+                                               last_name: person.last_name,
+                                               company_name: person.company_name,
+                                               zip_code: person.zip_code,
+                                               birthday: person.birthday })
+      
+      next if person == doublette || doublette_already_exists?(person, doublette)
 
+      Person::Doublette.create!(person_1: person, person_2: doublette)
+    end
+  end
+
+  private
+
+  def person_doublette_finder
+    @person_doublette_finder ||= Import::PersonDoubletteFinder.new
+  end
+
+  def doublette_already_exists?(person_1, person_2)
+    Person::Doublette.where(person_1: person_1, person_2: person_2)
+      .or(Person::Doublette.where(person_2: person_1, person_1: person_2)).any?
   end
 end
--- spec/domain/people/doublette_checker_spec.rb
+++ spec/domain/people/doublette_checker_spec.rb
@@ -6,8 +6,34 @@ describe People::DoubletteChecker do
   subject { checker.check }
 
   context 'check' do
-    it 'creates doublet when attributes match' do
-      expect { subject }.to change(PersonDoublette.count).by(1)
+    it 'creates one doublette when attributes match once' do
+      create_duplicate(:top_leader)
+      expect { subject }.to change { Person::Doublette.count }.by(1)
     end
+
+    it 'creates two doublettes when attributes match twice' do
+      create_duplicate(:top_leader)
+      create_duplicate(:top_leader)
+      expect { subject }.to change { Person::Doublette.count }.by(2)
+    end
+
+    it 'creates no doublette when doublet already exists' do
+      duplicate = create_duplicate(:top_leader)
+      Person::Doublette.create(person_1: duplicate, person_2: people(:top_leader))
+      expect { subject }.to_not change { Person::Doublette.count }
+    end
+
+    it 'creates no doublette when no attributes match' do
+      expect { subject }.to_not change { Person::Doublette.count }
+    end
+  end
+
+  private
+
+  def create_duplicate(name)
+    duplicate = people(name).dup
+    duplicate.email = nil
+    duplicate.save!
+    duplicate
   end
 end
